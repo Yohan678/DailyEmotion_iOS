5e5eef5792f983938380227897fc9d2279200717
Yohan Yoon <beginnerdevyohan@gmaill.com>
2026-05-22 02:16:21 -0400

    NewEmotionView Created

---
+++ DailyEmotion_iOS/NewEmotionView.swift
@@ -0,0 +1,18 @@
+//
+//  NewEmotionView.swift
+//  DailyEmotion_iOS
+//
+//  Created by Yohan Yoon on 5/22/26.
+//
+
+import SwiftUI
+
+struct NewEmotionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewEmotionView()
+}
